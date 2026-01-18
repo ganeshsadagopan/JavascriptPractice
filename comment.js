2507f7e96e11ac85f1cd21d4e5778ec7c0ce9451
Ganesh Sadagopan <sadagopanganes64@gmail.com>
2026-01-18 17:24:23 +0530

    simple js code

---
+++ comment.js
@@ -0,0 +1,14 @@
+
+// console.log("This is a JavaScript program with sinle line comments.");
+
+
+
+/*
+
+   console.log("This is a JavaScript program with multi-line comments.");
+
+   console.log("This is a JavaScript program with multi-line comments.");
+
+   console.log("This is a JavaScript program with multi-line comments.");
+
+*/
--- hello.js
+++ hello.js
@@ -1,2 +1,3 @@
 
 console.log("hello, world!")
+// This is a simple JavaScript program that prints "hello, world!" to the console.
