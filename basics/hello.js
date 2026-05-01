bca344d84cc72a5190a64c2e06d623345144bbf7
Ganesh Sadagopan <sadagopanganes64@gmail.com>
2026-05-01 12:28:36 +0530

    console log

--- basics/hello.js
+++ basics/hello.js
@@ -2,3 +2,8 @@
 console.log("hello, world!")
 // This is a simple JavaScript program that prints "hello, world!" to the console.
 
+let a=10
+console.log("this value of a is: " + a)
+console.log(`this value of a is: ${a}`)
+// In JavaScript, you can use string concatenation with the + operator or template literals (using backticks) to include variables in strings.
+
